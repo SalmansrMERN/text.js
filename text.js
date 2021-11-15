04baf122622b3adab8a38da45b8986fd78d450b2
Salman S. Rahman <salmansr1995@gmail.com>
2021-11-15 20:25:27 +0600

    first commit

---
+++ text.js
@@ -0,0 +1,23 @@
+var firstName = "taskin";
+var lastName = "asghar";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName.toUpperCase())
+
+
+var thirdName = "TASKIN";
+var fourthName = "ASGHAR";
+
+var fullNamee = thirdName + " " + fourthName;
+
+console.log(fullNamee.toLowerCase())
+
+
+var num1 = 374.991991342;
+var num2 = 287.111711111;
+
+var total = (num1 + num2);
+
+console.log(typeof (Number(total.toFixed(2))))
+
